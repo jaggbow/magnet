beba0b2e37ab17b71a4bc1effeeea6fdaf21fa18
jaggbow <oussama.boussif@student.ecp.fr>
2022-07-27 15:49:51 -0400

    addef configs for both fno2d and magnet_cnn_2d

--- models/factory.py
+++ models/factory.py
@@ -1,13 +1,17 @@
 from .fno_1d import FNO1d
+from .fno_2d import FNO2d
 from .magnet_cnn import MAgNetCNN
+from .magnet_cnn_2d import MAgNetCNN_2d
 from .magnet_cnn_no_interaction import MAgNetCNN_no_interaction
 from .magnet_gnn import MAgNetGNN
 from .mpnn import MPNN
 
 FACTORY = {
     'fno_1d': FNO1d,
+    'fno_2d': FNO2d,
     'mpnn': MPNN,
     'magnet_cnn_no_interaction': MAgNetCNN_no_interaction,
     'magnet_cnn': MAgNetCNN,
+    'magnet_cnn_2d': MAgNetCNN_2d,
     'magnet_gnn': MAgNetGNN
 }
